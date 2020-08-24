0aa0adee475f52878b4367d24982904bfab89d7b
Mahir-ally1 <55910983+Mahir-ally1@users.noreply.github.com>
2020-08-24 23:21:52 +0530

    Update Parallel_Tempering.py

--- Bayesian/Parallel_Tempering.py
+++ Bayesian/Parallel_Tempering.py
@@ -48,13 +48,13 @@ batch_size = 100
 # number of epochs to train the model
 n_epochs = 1
 
-lrate = 0.01
+lrate = 0.02
 burnin = 0.25
 ulg = True
 no_channels = 1
 size_train = 1200
 size_test = 1000
-step_size = 0.05
+step_size = 0.005
 num_chains = 8 #equal to no of cores available
 pt_samples = 0.50
 langevin_step = 100
