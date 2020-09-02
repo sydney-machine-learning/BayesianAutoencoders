36fdc17509902824d7e8838445402c84a1dc2d68
Mahir-ally1 <55910983+Mahir-ally1@users.noreply.github.com>
2020-09-02 22:43:11 +0530

    Update Parallel_Tempering_Tabular.py

--- Bayesian/Parallel_Tempering_Tabular.py
+++ Bayesian/Parallel_Tempering_Tabular.py
@@ -71,7 +71,7 @@ enc_shape= 2
 
 def data_load(data='train'):
     if data == 'test':
-        test_data = make_swiss_roll(1600)
+        test_data = make_swiss_roll(1600,0.05)
         test_data = torch.stack([torch.from_numpy(np.array(i)) for i in test_data])
         print(test_data)
         test_data, _ = torch.utils.data.random_split(test_data, [size_test, len(test_data) - size_test])
@@ -79,7 +79,7 @@ def data_load(data='train'):
         test_data = torch.stack(test_data).to(device)
         return test_data
     else:
-        train_data = make_swiss_roll(1600)
+        train_data = make_swiss_roll(1600,0.05)
         train_data = torch.stack([torch.from_numpy(np.array(i)) for i in train_data])
         print(train_data)
         train_data, _ = torch.utils.data.random_split(train_data, [size_train, len(train_data) - size_train])
