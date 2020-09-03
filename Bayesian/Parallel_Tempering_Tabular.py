9c9fb6bc61e63b4553ca5b7a4d0f7047fee51a0c
Mahir-ally1 <55910983+Mahir-ally1@users.noreply.github.com>
2020-09-03 12:13:50 +0530

    Update Parallel_Tempering_Tabular.py

--- Bayesian/Parallel_Tempering_Tabular.py
+++ Bayesian/Parallel_Tempering_Tabular.py
@@ -72,13 +72,13 @@ enc_shape= 2
 
 def data_load(data='train'):
     if data == 'test':
-        test_data, colors = make_swiss_roll(1600)
+        test_data, colors = make_swiss_roll(no_samples,noise)
         test_data = torch.Tensor(test_data)
        # test_data, _ = torch.utils.data.random_split(test_data, [size_test, len(test_data) - size_test])
         #test_data = MinMaxScaler().fit_transform(test_data)
         return test_data
     else:
-        train_data = make_swiss_roll(1600)
+        train_data = make_swiss_roll(no_samples,noise)
         train_data = torch.Tensor(train_data)
         #train_data, _ = torch.utils.data.random_split(train_data, [size_train, len(train_data) - size_train])
         #train_data = MinMaxScaler().fit_transform(train_data)
