6864f14110a6a9d3646110efd7c85590242a6643
Mahir-ally1 <55910983+Mahir-ally1@users.noreply.github.com>
2020-09-05 19:52:50 +0530

    Update Parallel_Tempering_Tabular.py

--- Bayesian/Parallel_Tempering_Tabular.py
+++ Bayesian/Parallel_Tempering_Tabular.py
@@ -415,9 +415,9 @@ class ptReplica(multiprocessing.Process):
 
             sum_value = diff_likelihood + diff_prior + diff_prop
             #print(sum_value)
-            sum_value*=-0.00001
+            sum_value*=-0.0001
             print(sum_value)
-            u = random.uniform(0.5, 1)
+            u = random.uniform(0,0.1)
             print(u)
 
             sum_value_array[i] = sum_value
